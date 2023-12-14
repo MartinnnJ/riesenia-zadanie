1d3660767e329d92782e99c71a85b6b0c2263c4a
Martin Jancura <martin.jancura@odyzeo.com>
2023-12-14 19:48:23 +0100

    pridane scroll animacie

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+'use strict';
+
+const headerElements = Array.from(document.querySelectorAll('.header .md'));
+const mainElements = Array.from(document.querySelectorAll('.main .md'));
+const footerElements = Array.from(document.querySelectorAll('.footer .md'));
+
+for (const el of headerElements) {
+  el.classList.remove('md');
+}
+
+// observer init
+const options = {
+  root: null,
+  rootMargin: '0px',
+  treshold: 1.0,
+};
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove('md');
+    }
+  });
+}, options);
+
+for (const el of mainElements) {
+  observer.observe(el); // .observe method can observe only one element
+}
+
+for (const el of footerElements) {
+  observer.observe(el);
+}
