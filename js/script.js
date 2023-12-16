3aca96e73b1caf86e8518a786168d08e8b1e3908
Martin Jancura <martin.jancura@odyzeo.com>
2023-12-16 18:35:10 +0100

    pridany modal

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@
 const headerElements = Array.from(document.querySelectorAll('.header .md'));
 const mainElements = Array.from(document.querySelectorAll('.main .md'));
 const footerElements = Array.from(document.querySelectorAll('.footer .md'));
+const buttons = Array.from(document.querySelectorAll('.header .button, .main .button'));
+const modalCloseButton = document.querySelector('.close-button');
+const modal = document.querySelector('.modal');
 
+for (const button of buttons) {
+  button.addEventListener('click', () => {
+    modal.showModal();
+  })
+}
+
+modalCloseButton.addEventListener('click', () => {
+  modal.close();
+})
+
+// header transitions animations
 for (const el of headerElements) {
   el.classList.remove('md');
 }
@@ -23,10 +37,12 @@ const observer = new IntersectionObserver(entries => {
   });
 }, options);
 
+// main transitions animations
 for (const el of mainElements) {
   observer.observe(el); // .observe method can observe only one element
 }
 
+// footer transitions animations
 for (const el of footerElements) {
   observer.observe(el);
 }
